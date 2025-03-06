bbbc44f2ac1a41ac3cfde93c6dd0066d1f24a924
Дмитрий Крохалев <Krohalevdmitrij934@gmail.com>
2025-03-06 20:53:35 +0500

    Структура данных и работа с ними

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -8,18 +8,28 @@ import TripSortView from '../view/trip-sort-view.js';
 export default class TripPresenter {
   eventListComponent = new EventListView();
 
-  constructor({tripContainer}) {
+  constructor({tripContainer, pointsModel}) {
     this.tripContainer = tripContainer;
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    this.eventsListPoints = [...this.pointsModel.getPoints()];
+
     render(new TripSortView(), this.tripContainer);
     render(this.eventListComponent, this.tripContainer);
     render(new EventEditView, this.eventListComponent.getElement());
     render(new EventCreateView(), this.eventListComponent.getElement());
-
     for (let i = 0; i < 3; i++) {
       render(new EventView(), this.eventListComponent.getElement());
+    } 
+    for (let i = 1; i < this.eventsListPoints.length; i++){
+      const point = new this.EventEditView({
+        point: this.eventsListPoints[i],
+        offers: [...this.pointsModel.getOffersById(this.eventsListPoints[i].type, this.eventsListPoints[i])],
+        destination:this.pointsModel.getDestinationsById(this.eventsListPoints[i].destination)
+      });
+      render(point,this.eventListComponent.getElement());
     }
   }
 }
