33f597f3e47757b9995875d8256e3c95b626fc11
Дмитрий Крохалев <Krohalevdmitrij934@gmail.com>
2025-04-01 22:19:10 +0500

    проверка

--- src/view/trip-filters-view.js
+++ src/view/trip-filters-view.js
@@ -1,23 +1,7 @@
 import AbstractView from "../framework/view/abstract-view.js";
 
 function createFiltersTemplate() {
-  return `<div class="page-body__container  page-header__container">
-        <img class="page-header__logo" src="img/logo.png" width="42" height="42" alt="Trip logo">
-
-        <div class="trip-main">
-          <section class="trip-main__trip-info  trip-info">
-            <div class="trip-info__main">
-              <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
-
-              <p class="trip-info__dates">18&nbsp;&mdash;&nbsp;20 Mar</p>
-            </div>
-
-            <p class="trip-info__cost">
-              Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
-            </p>
-          </section>
-
-          <div class="trip-main__trip-controls  trip-controls">
+  return `<div class="trip-main__trip-controls  trip-controls">
             <div class="trip-controls__filters">
               <h2 class="visually-hidden">Filter events</h2>
               <form class="trip-filters" action="#" method="get">
