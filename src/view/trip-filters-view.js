9a88e7c945962ab6394f60c3c24e4e46e6bc32fc
Дмитрий Крохалев <Krohalevdmitrij934@gmail.com>
2025-04-01 22:44:59 +0500

    доделка

--- src/view/trip-filters-view.js
+++ src/view/trip-filters-view.js
@@ -1,7 +1,10 @@
 import {createElement} from '../render.js';
 
 function createFiltersTemplate() {
-  return `<form class="trip-filters" action="#" method="get">
+  return `<div class="trip-main__trip-controls  trip-controls">
+            <div class="trip-controls__filters">
+              <h2 class="visually-hidden">Filter events</h2>
+              <form class="trip-filters" action="#" method="get">
                 <div class="trip-filters__filter">
                   <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything">
                   <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
@@ -23,7 +26,9 @@ function createFiltersTemplate() {
                 </div>
 
                 <button class="visually-hidden" type="submit">Accept filter</button>
-              </form>`
+              </form>
+            </div>
+          </div>`
 };
 
 export default class TripFiltersView {
