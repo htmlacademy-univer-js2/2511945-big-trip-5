726c42d5ae876d7baa45c92f6c8460c2e76cdad5
Дмитрий Крохалев <Krohalevdmitrij934@gmail.com>
2025-05-21 14:57:24 +0500

    доделка

--- src/mock/sort.js
+++ src/mock/sort.js
@@ -4,11 +4,11 @@ export const SORT_TYPES = {
     PRICE: 'sort-price'
   };
   
-  export const generateSortItems = () => ([
-    { type: SORT_TYPES.DAY, name: 'Day', isDisabled: false },
-    { type: 'sort-event', name: 'Event', isDisabled: true },
-    { type: SORT_TYPES.TIME, name: 'Time', isDisabled: false },
-    { type: SORT_TYPES.PRICE, name: 'Price', isDisabled: false, isChecked: true },
-    { type: 'sort-offer', name: 'Offers', isDisabled: true }
-  ]);
+ export const generateSortItems = () => ([
+  { type: SORT_TYPES.DAY, name: 'Day', isDisabled: false, isChecked: true },
+  { type: 'sort-event', name: 'Event', isDisabled: true },
+  { type: SORT_TYPES.TIME, name: 'Time', isDisabled: false },
+  { type: SORT_TYPES.PRICE, name: 'Price', isDisabled: false },
+  { type: 'sort-offer', name: 'Offers', isDisabled: true }
+]);
   
