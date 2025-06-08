d8edf890448c60f352fb3cdfd63155f2820cdaa3
Дмитрий Крохалев <Krohalevdmitrij934@gmail.com>
2025-06-08 18:28:52 +0500

    взаимодействие с сервером

--- src/model/filter-model.js
+++ src/model/filter-model.js
@@ -1,11 +1,23 @@
-export default class FilterModel {
-  #filter = 'everything';
+import {FilterType} from '../const.js';
+
+export default class FiltersModel {
+  #filter = FilterType.EVERYTHING;
+  #observers = [];
 
   get filter() {
     return this.#filter;
   }
 
-  set filter(newFilter) {
-    this.#filter = newFilter;
+  setFilter(filter) {
+    this.#filter = filter;
+    this.#notifyObservers();
+  }
+
+  addObserver(observer) {
+    this.#observers.push(observer);
+  }
+
+  #notifyObservers() {
+    this.#observers.forEach((observer) => observer());
   }
 }
